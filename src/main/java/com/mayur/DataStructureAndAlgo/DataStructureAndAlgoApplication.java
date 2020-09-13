90f40fc74b95bea2e5bda9e735420087447eca2f
Mayur Mishra <mayur.mishra@gdn-commerce.com>
2020-09-13 16:10:03 +0530

    first commit

---
+++ src/main/java/com/mayur/DataStructureAndAlgo/DataStructureAndAlgoApplication.java
@@ -0,0 +1,13 @@
+package com.mayur.DataStructureAndAlgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructureAndAlgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructureAndAlgoApplication.class, args);
+	}
+
+}
